895a6f9d40a0633947575063bd96fe0ea512999d
Wako-Harada <wako.harada.0709@gmail.com>
2025-09-18 19:26:36 +0900

    create

---
+++ laravel/resources/js/components/MemoButton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Plus from "@/components/svgs/PlusSvg.vue";
+</script>
+
+<template>
+<button class="w-11/12 bg-orange-300 rounded-md gap-2 flex flex-row justify-center items-center p-2 m-2 text-white">
+    <Plus class="text-white"/>
+    メモを保存
+</button>
+</template>
+
+<style scoped>
+
+</style>
