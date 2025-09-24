a17b8ae15a9b825e674a6b6a288bbf411eb90d71
Wako-Harada <wako.harada.0709@gmail.com>
2025-09-24 11:25:46 +0900

    index

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MemoController;
 
 Route::post('/memos',[MemoController::class,'store']);
+Route::get ('/memos',[MemoController::class,'index']);
